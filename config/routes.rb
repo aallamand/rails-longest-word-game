6b57cc28777963204f13b19238ead637055e95c5
Arthur Allamand <a.allamand@gmail.com>
2017-08-07 17:13:25 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'gaming/game'
+  get 'game', to: 'gaming#game'
+
+  get 'gaming/score'
+  get 'score', to: 'gaming#score'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
